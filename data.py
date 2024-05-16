e0ec553377251f240e89e9ec89b3b3f93b81e7b1
molereddy <m.anmolreddy@gmail.com>
2024-05-16 21:01:51 +0000

    refactor, load pavlick dataset

--- data.py
+++ data.py
@@ -10,21 +10,66 @@ def set_seed(seed):
     np.random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-def read_gyfac_split(data_path, split='train'):
-    texts, labels = [], []
-    for label in [0, 1]:
-        file = open(os.path.join(data_path, f"{split}_{label}.txt"))
-        sentences = [sentence.strip() for sentence in file.readlines()]
-        print(f"{len(sentences)} examples with label {label} in split {split}")
-        texts += sentences
-        labels += [label for _ in sentences]
-        file.close()
-    combined = list(zip(texts, labels))
-    random.shuffle(combined)
-    texts, labels = zip(*combined)
-    return texts, labels
+def prepare_pavlick_formality(tokenizer, refresh=False):
+    def preprocess_dataset_cols(dataset):
+        dataset = dataset.rename_column("avg_score", "label")
+        dataset = dataset.remove_columns(["domain", "sentence"])
+        return dataset
 
-def prepare_split(data_path, split, tokenizer, cut_k=0):
+    def preprocess_tokenize(examples):
+        return tokenizer(examples['sentence'], padding='max_length', truncation=True)
+
+    data_path = "/work/pi_dhruveshpate_umass_edu/project_18/pavlick"
+
+    if os.path.exists(data_path) and not refresh:
+        print("loading from saved dataset")
+        train_dataset = load_from_disk(os.path.join(data_path, "train_dataset"))
+        val_dataset = load_from_disk(os.path.join(data_path, "val_dataset"))
+        test_dataset = load_from_disk(os.path.join(data_path, "test_dataset"))
+    else:
+        print("preparing split newly")
+        dataset = load_dataset("osyvokon/pavlick-formality-scores")
+        dataset = dataset.map(preprocess_tokenize, batched=True)
+        dataset = preprocess_dataset_cols(dataset)
+        for split in dataset.keys():
+            print(f"Attributes in {split} split: {dataset[split].column_names}")
+        dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+        
+        train_dataset = dataset['train'].shuffle(seed=42)
+        test_dataset = dataset['test']
+        eval_splits = test_dataset.train_test_split(test_size=0.5, shuffle=True, seed=seed)
+        test_dataset = eval_splits["train"]
+        val_dataset = eval_splits["test"]
+        
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+        train_dataset.save_to_disk(os.path.join(data_path, "train_dataset"))
+        val_dataset.save_to_disk(os.path.join(data_path, "val_dataset"))
+        test_dataset.save_to_disk(os.path.join(data_path, "test_dataset"))
+    
+    print("Train Dataset:", train_dataset)
+    print("Validation Dataset:", val_dataset)
+    print("Test Dataset:", test_dataset)
+    
+    return train_dataset, val_dataset, test_dataset
+
+
+def prepare_gyfac_split(data_path, split, tokenizer, cut_k=0):
+    
+    def read_gyfac_split(data_path, split='train'):
+        texts, labels = [], []
+        for label in [0, 1]:
+            file = open(os.path.join(data_path, f"{split}_{label}.txt"))
+            sentences = [sentence.strip() for sentence in file.readlines()]
+            print(f"{len(sentences)} examples with label {label} in split {split}")
+            texts += sentences
+            labels += [label for _ in sentences]
+            file.close()
+        combined = list(zip(texts, labels))
+        random.shuffle(combined)
+        texts, labels = zip(*combined)
+        return texts, labels
+    
     texts, labels = read_gyfac_split(data_path, split)
     data_dict = {"text": texts, "label": labels}
     dataset = Dataset.from_dict(data_dict)
@@ -61,21 +106,22 @@ def prepare_split(data_path, split, tokenizer, cut_k=0):
 
 
 def load_gyfac(tokenizer, data_path = "/work/pi_dhruveshpate_umass_edu/project_18/gyfac_pilot", seed=1, val_split=0.05, cut_k=0, refresh=False):
+    
     directory = f"{seed}_{val_split}_{cut_k}"
     full_path = os.path.join(data_path, directory)
     
-    if os.path.exists(full_path) or not refresh:
+    if os.path.exists(os.path.join(full_path, "train_dataset")) and not refresh:
         print("loading from saved dataset")
         train_dataset = load_from_disk(os.path.join(full_path, "train_dataset"))
         val_dataset = load_from_disk(os.path.join(full_path, "val_dataset"))
         test_dataset = load_from_disk(os.path.join(full_path, "test_dataset"))
     else:
         print("preparing split newly")
-        full_train_dataset = prepare_split(data_path, "train", tokenizer, cut_k)
+        full_train_dataset = prepare_gyfac_split(data_path, "train", tokenizer, cut_k)
         train_splits = full_train_dataset.train_test_split(test_size=val_split, shuffle=True, seed=seed)
         train_dataset = train_splits["train"]
         val_dataset = train_splits["test"]
-        test_dataset = prepare_split(data_path, "test", tokenizer, cut_k)
+        test_dataset = prepare_gyfac_split(data_path, "test", tokenizer, cut_k)
         
         if not os.path.exists(full_path):
             os.makedirs(full_path)
@@ -96,7 +142,7 @@ def main():
     set_seed(seed)
     
     hf_key = "t5-base"
-    tokenizer = T5Tokenizer.from_pretrained(hf_key, use_fast=True, legacy=True, legacy=True)
+    tokenizer = T5Tokenizer.from_pretrained(hf_key, use_fast=True, legacy=True)
     data_path = "/work/pi_dhruveshpate_umass_edu/project_18/gyfac_pilot"
     train_dataset, val_dataset, test_dataset = load_gyfac(tokenizer, data_path, seed, val_split, cut_k)
 
